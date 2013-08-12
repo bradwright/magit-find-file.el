57f9e67c9037545d3f42ed5a558495b8c8b07ed3
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-12 21:36:07 +0100

    Initial commit of magit-find-file
    README explaning installation, and basic package.

---
+++ README.md
@@ -0,0 +1,47 @@
+# magit-find-file
+
+[magit](https://github.com/magit/magit) powered equivalent of
+TextMate's `cmd-t` functionality (or
+[ctrlp.vim](https://github.com/kien/ctrlp.vim), if that's your bag).
+
+Depends on Magit.
+
+## Installation
+
+Install it from [Melpa](http://melpa.milkbox.net/), or drop `magit-find-file.el` into your load path and add:
+
+```emacs
+(require 'magit-find-file)
+```
+
+into your `init.el`.
+
+## Finding files
+
+Invoking `magit-find-file-completing-read` will give you a list of
+candidates as ascertained by `git ls-files`. So basically all the
+files you should care about. It includes staged and not-yet-added
+files in the list.
+
+You may want to add it as a key command:
+
+```emacs
+(global-set-key (kbd "C-c p") 'magit-find-file-completing-read)
+```
+
+## Licence
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
---
+++ magit-find-file.el
@@ -0,0 +1,84 @@
+;;; magit-find-file.el --- completing-read over all files in Git
+
+;; Copyright (C) 2013 Bradley Wright
+
+;; Author: Bradley Wright <brad@intranation.com>
+;; Keywords: git
+;; URL: https://github.com/bradleywright/magit-find-file.el
+;; Version: 0.0.1
+;; Package-Requires: ((magit "1.2.0"))
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a Git-powered replacement for something like TextMate's
+;; Cmd-t.
+
+;; Uses a configurable completing-read to open any file in the
+
+;; Git repository of the current buffer.
+
+;; Usage:
+;;
+;;     (require 'magit-find-file) ;; if not using the ELPA package
+;;     (global-set-key (kbd "C-c p") 'magit-find-file-completing-read)
+;;
+;; Customize 'magit-completing-read-function to change the completing
+;; read engine used (so it should behave like Magit does for you).
+;;
+;; Customize 'magit-find-file-skip-images to include images in your
+;; candidate list. This is t by default.
+
+;;; Code:
+
+(defgroup magit-find-file nil
+  "Use Magit to completing-read over files"
+  :prefix "magit-find-file-"
+  :group 'tools)
+
+(defcustom magit-find-file-skip-images t
+  "Skip images in completing-read candidate list."
+  :group 'magit-find-file
+  :type 'boolean)
+
+(defun magit-find-file-is-image (name)
+  "Identifies images by extension."
+  (if magit-find-file-skip-images
+      (member (file-name-extension name) '("jpg" "png" "gif" "jpeg"))
+    nil))
+
+;;;###autoload
+(defun magit-find-file-completing-read ()
+  "Uses a completing read to open a file from git ls-files"
+  (interactive)
+  (let ((default-directory (magit-get-top-dir)))
+    (find-file
+     (magit-completing-read
+      (format "Find file: %s" (abbreviate-file-name default-directory))
+      (remove-if 'magit-find-file-is-image (magit-git-lines "ls-files" "--exclude-standard" "-co"))))))
+
+(provide 'magit-find-file)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; mangle-whitespace: t
+;; require-final-newline: t
+;; checkdoc-minor-mode: t
+;; End:
+
+;;; magit-find-file.el ends here
