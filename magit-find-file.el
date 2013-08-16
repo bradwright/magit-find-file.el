fffc3c51811230459a235f6474dd543733baedec
Bradley Wright <brad@intranation.com>
2013-08-16 20:50:45 +0100

    Version 1.0.0
    It's stable, I've been using it for a while now.

--- magit-find-file.el
+++ magit-find-file.el
@@ -5,7 +5,7 @@
 ;; Author: Bradley Wright <brad@intranation.com>
 ;; Keywords: git
 ;; URL: https://github.com/bradleywright/magit-find-file.el
-;; Version: 0.0.1
+;; Version: 1.0.0
 ;; Package-Requires: ((magit "1.2.0"))
 
 ;; This file is not part of GNU Emacs.
