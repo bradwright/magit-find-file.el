5a909a2d4c7ccbc1d20a961406e2fad68a24ae63
Bradley Wright <brad@intranation.com>
2013-08-25 22:56:23 +0100

    Require magit
    Without this, the magit-get-top-dir call will fail unless you already
    have magit loaded.

--- magit-find-file.el
+++ magit-find-file.el
@@ -5,7 +5,7 @@
 ;; Author: Bradley Wright <brad@intranation.com>
 ;; Keywords: git
 ;; URL: https://github.com/bradleywright/magit-find-file.el
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((magit "1.2.0"))
 
 ;; This file is not part of GNU Emacs.
@@ -45,6 +45,7 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'magit)
 
 (defgroup magit-find-file nil
   "Use Magit to completing-read over files"
