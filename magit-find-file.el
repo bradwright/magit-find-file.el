ff2eda0523d0b42193645bd77cfcb4b857f528f3
Jonas Bernoulli <jonas@bernoul.li>
2014-09-04 15:15:00 +0200

    pimp commentary

--- magit-find-file.el
+++ magit-find-file.el
@@ -36,10 +36,10 @@
 ;;     (require 'magit-find-file) ;; if not using the ELPA package
 ;;     (global-set-key (kbd "C-c p") 'magit-find-file-completing-read)
 ;;
-;; Customize 'magit-completing-read-function to change the completing
+;; Customize `magit-completing-read-function' to change the completing
 ;; read engine used (so it should behave like Magit does for you).
 ;;
-;; Customize 'magit-find-file-skip-images to include images in your
+;; Customize `magit-find-file-skip-images' to include images in your
 ;; candidate list. This is t by default.
 
 ;;; Code:
